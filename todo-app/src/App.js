e2c2a5508dedec2ac22c5f2fc3b1b3d9be4c63dd
vrushabhdhatrak <vrushabhdhatral10@gmail.com>
2021-04-09 21:26:37 +0530

    all completed

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,33 +1,85 @@
-
-import './App.css';
-import TextField from '@material-ui/core/TextField';
-import { useState } from 'react';
+import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import { useState, useEffect } from "react";
+import { Button } from "@material-ui/core";
+import { db } from "./firebase.config";
+import firebase from "firebase";
+import TodoListItem from "./Todo";
 
 function App() {
-  const [taskInput, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [todoInput, setTodoInput] = useState("");
 
-  function addTask(){
-    
-  } 
+  useEffect(() => {
+    getTodos();
+  }, []); // blank to run only on first launch
+
+  function getTodos() {
+    db.collection("todos").onSnapshot(function (querySnapshot) {
+      setTodos(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          todo: doc.data().todo,
+          inprogress: doc.data().inprogress,
+        }))
+      );
+    });
+  }
+
+  function addTodo(e) {
+    e.preventDefault();
+
+    db.collection("todos").add({
+      inprogress: true,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      todo: todoInput,
+    });
+
+    setTodoInput("");
+  }
 
   return (
-    <div className="App" 
-    style=
-    {{
-        display:"flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        alignItems: "center"
+    <div className="App">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          
+          
+        }}
+      >
+        <h1>😃😎 Todos App 😃😎</h1>
+        <form>
+          <TextField
+            id="standard-basic"
+            label="Write a Todo"
+            value={todoInput}
+            style={{ width: "90vw", maxWidth: "500px" }}
+            onChange={(e) => setTodoInput(e.target.value)}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            onClick={addTodo}
+            style={{ display: "none" }}
+          >
+            Default
+          </Button>
+        </form>
 
-
-      }}>
-      <h1>Todo App...!!!</h1>
-      <TextField 
-      id="standard-basic" 
-      value = {taskInput}
-      onChange = {(e) => setTask(e.target.value)}
-      label="Write your task...!!"  
-      style={{maxWidth: "200px"}}/>
+        <div style={{ width: "90vw", maxWidth: "500px", marginTop: "24px" }}>
+          {todos.map((todo) => (
+            <TodoListItem
+              todo={todo.todo}
+              inprogress={todo.inprogress}
+              id={todo.id}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }

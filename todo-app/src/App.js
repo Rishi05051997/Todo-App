5a4f47eda0ee6df901e9a749dfb8092840b57663
vrushabhdhatrak <vrushabhdhatral10@gmail.com>
2021-04-09 17:31:55 +0530

    connected firebase to project with todo field

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,24 @@
 
 import './App.css';
+import TextField from '@material-ui/core/TextField';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" 
+    style=
+    {{
+        display:"flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center"
+        
+
+      }}>
       <h1>Todo App...!!!</h1>
+      <TextField id="standard-basic" label="Write your task...!!"  style={{maxWidth: "200px"}}/>
     </div>
   );
 }
 
 export default App;
+
